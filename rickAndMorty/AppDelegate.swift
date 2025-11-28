9794d0109296a6c4fca6832065ef3deb4c3092c6
jaqueline.sanchez <jaqueline.sanchez@gonet.us>
2025-11-27 23:11:19 -0600

    Initial commit

---
+++ rickAndMorty/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  rickAndMorty
+//
+//  Created by Jaqueline Sanchez lopez on 27/11/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ rickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rickAndMorty
+//
+//  Created by Jaqueline Sanchez lopez on 27/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
